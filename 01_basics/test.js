88946d7b9666321a028f2bd4def6d34aad686def
narottamnarayan <116140933+narottamnarayan@users.noreply.github.com>
2024-01-29 04:44:49 +0000

    container config done

---
+++ 01_basics/test.js
@@ -0,0 +1,11 @@
+const accountId = 236712
+let accountEmail = "narottam@google.com"
+var accountPassword = "1223456"
+accountCity = "jaipur"
+
+// accountId = 234864 //this is not allowed
+
+console.log( typeof "accountId")
+
+// 
+console.table([accountId,accountEmail,accountPassword,accountCity])
