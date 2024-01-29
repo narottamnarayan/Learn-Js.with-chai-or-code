2fc2eac9fb72605b57ce42f8bbf3bd33ec4623ee
narottamnarayan <116140933+narottamnarayan@users.noreply.github.com>
2024-01-29 08:40:05 +0000

    conversation of dataTypes

---
+++ 01_basics/03_conversationOperation.js
@@ -0,0 +1,35 @@
+let score = "narottam"
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+
+let isLoggedIn = "narottam"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => ture; 0 => false
+//"" => false
+//"narottam" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log( typeof stringNumber);
