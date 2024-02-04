68fcdd3173669aa3a6c24a630b9182812f6baf31
narottamnarayan <116140933+narottamnarayan@users.noreply.github.com>
2024-02-04 03:34:05 +0000

    update data config

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// // console.log(2 = 1);
+// console.log(2 <= 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null == 1);
+// console.log(null >= 1);
+// console.log(null < 1);
+// console.log(null <= 1);
+
+// console.log(undefined == 1);
+// console.log(undefined >= 1);
+// console.log(undefined < 1);
+// console.log(undefined <= 1);
+
+//avoid this type of conversation--------//
+
+// ===
+console.log("2" === 1);
