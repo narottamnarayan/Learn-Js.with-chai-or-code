9dae67eea540be0fa8dc1565784b0cb037867817
narottamnarayan <116140933+narottamnarayan@users.noreply.github.com>
2024-02-08 03:10:40 +0000

    datatypes summery

---
+++ 01_basics/datatype_summery.js
@@ -0,0 +1,43 @@
+// ----primitive------ //
+
+// 7 type of data;
+
+// String, Number, Boolean, BigInt, null, undefined, Symbol, 
+
+const score = 100;
+const scoreValue = 100.3
+
+const isloggedin = false
+const outsidetemp = null
+
+let userEmail;
+
+const id = Symbol("123")
+
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 48168476874998287487128n
+
+// console.log( typeof bigNumber);
+
+
+
+// ----------------Reference type (Non-primitive)----------------//
+
+// Array, Object, Function,
+
+const heros = ["shaktiman", "doga" , "naagraj"]
+
+ let myObj = {
+    name: "narottam",
+    age: 24,
+}
+
+
+ const myFunction =  function(){
+    console.log("hello world")
+ }
+  
+ console.log(typeof isloggedin);
