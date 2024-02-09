e0a7008f40f073e4a52520ef6c0c97e1e4f55d79
narottamnarayan <116140933+narottamnarayan@users.noreply.github.com>
2024-02-09 03:35:32 +0000

    string data

---
+++ 01_basics/05_string.js
@@ -0,0 +1,41 @@
+const name = "narottam"
+
+const repocount = 50
+
+// console.log(name +  repocount + " narayan");
+
+console.log(`hi i am ${name} this is my  first ${repocount}`);
+
+
+// const gameName = "gta5"
+
+// console.log(gameName);
+
+const GameName = new String( "battale ground");
+
+// console.log(GameName.__proto__);
+
+// console.log(GameName.length);
+
+// console.log(GameName.toUpperCase());
+
+console.log(GameName.charAt(4));
+
+console.log(GameName.indexOf('g'));
+
+const newGame = GameName.substring(0, 7);
+console.log(newGame);
+
+const anOthergame = GameName.slice(1, 6)
+console.log(anOthergame);
+
+
+ const otherGame = "   narayan  "
+ console.log(otherGame);
+ console.log(otherGame.trim());
+
+
+ const url = "http//hitesh.com/hitesh%20chaudhary"
+
+ console.log(url.replace('%20', '_'));
+ console.log(url.includes('hitesh'));
