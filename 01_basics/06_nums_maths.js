ae1d271332e531de9ac5253470bb4cf3faa0df3b
narottamnarayan <116140933+narottamnarayan@users.noreply.github.com>
2024-02-12 03:40:47 +0000

    nums&maths done

---
+++ 01_basics/06_nums_maths.js
@@ -0,0 +1,37 @@
+const score = 100
+
+// console.log(score);
+
+const balance =  new Number(100.4576)
+
+// console.log(balance.toFixed(3));
+// console.log(balance.valueOf());
+
+// console.log (balance);
+// console.log(typeof(balance.toLocaleString()));
+
+const hundreds = 100000000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++++++ maths +++++++++++++++ //
+
+
+
+// console.log(Math.abs(-6)); //convert positive value
+// console.log(Math.round(7.42)); //convert in round
+// console.log(Math.ceil(5.6)); //max value show
+// console.log(Math.floor(5.6)); // min value show
+// console.log(Math.min(3, 8, 5, 9, 1)); // min num show
+// console.log(Math.max(3, 8, 5, 9, 1));  // max num show
+ 
+
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
