c1b5d4a64d426e873e8d58adff8856afdcd28f78
narottamnarayan <116140933+narottamnarayan@users.noreply.github.com>
2024-02-14 06:08:02 +0000

    arrays file done

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -2,4 +2,39 @@
 
 const arr = [0, 1, 2, 3, 4, 5, 6]
 
-console.log(arr[3]);
+const heros = new Array("saktiman", "goga", "nagraj")
+
+const arr2 = [7,3,5, 9, "narottam" , true, ]
+
+// console.log(arr[3]);
+// console.log(typeof arr2[5]);
+
+// methods----------- //
+
+// arr.push(8)
+// arr.pop()
+// arr.unshift(9)
+// arr.shift()
+// arr.concat(4)
+
+// console.log(arr.includes(5));
+// console.log(arr.indexOf(5));
+
+
+// const newArr = arr.join()
+
+// console.log(arr);
+// console.log(typeof newArr);
+
+// slice, splice-- //
+
+console.log("A", arr);
+
+const myN1 = arr.slice(1,4)
+console.log(myN1);
+
+console.log("B", arr);
+ 
+const myN2 = arr.splice(1,4)
+console.log(myN2);
+console.log('C', arr);
