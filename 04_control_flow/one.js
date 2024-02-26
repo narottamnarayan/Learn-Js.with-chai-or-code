bb3d2657305fc72cc3029bd42a12547b02c4dc6f
narottamnarayan <116140933+narottamnarayan@users.noreply.github.com>
2024-02-26 04:33:52 +0000

    first part done

---
+++ 04_control_flow/one.js
@@ -0,0 +1,50 @@
+// if
+
+const isuserloggedin = true
+const temperature = 41
+
+// if(temperature === 51){
+//    console.log("temperature is less then 50");
+// }else{
+
+//     console.log("temperature is greater then 50");
+// }
+// console.log("Execute");
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+
+// const score = 200
+
+// if(score > 100){
+//     let power = "fly"
+//     console.log(`user power: ${power}`);
+// }
+
+// console.log(`user power: ${power}`);
+
+// const balance = 1000
+
+// if(balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500){
+//     console.log("less then 500");
+// }else if(balance < 750){
+//     console.log("less then 750"); 
+// }else if(balance < 900) {
+//     console.log("less then 900");
+// }else{
+//     console.log("balance is less then 1200");
+// }
+
+// const userloggedin = true
+// const debitcard = true
+// const loggedinfromgoogle = true
+// const loggedinfromEmail = true
+
+// if(userloggedin && debitcard ){
+//     console.log("allow to buy course");
+// }
+// if(loggedinfromgoogle || loggedinfromEmail){
+//     console.log("user logged in")
+//  }
